c91ed50b1ae1cfa9b26ce65fc31ba7bbcd2bc47f
nharrisonau <nharrisonau@gmail.com>
2024-09-09 17:16:17 -0500

    changes

--- src/main.py
+++ src/main.py
@@ -1,17 +1,24 @@
 from fw_downloader import download_firmware
 from fw_unpacker import unpack_firmware
+import argparse
 
-def main(firmware_urls, save_path):
-    download_firmware(firmware_urls, save_path)
+def main(firmware_urls, save_path, n_samples):
+    download_firmware(firmware_urls, save_path, n_samples)
     unpack_firmware(save_path)
 
 if __name__ == '__main__':
-    # Three firmware samples
-    firmware_urls = [
-        "https://static.tp-link.com/TL-WR940N(US)_V4_160617_1476690524248q.zip",
-        "https://static.tp-link.com/resources/software/TL-WR1043ND_V1_140319.zip",
-        "https://static.tp-link.com/resources/software/TL-WA801ND_V1_130131_beta.zip"
-    ]
+    # Initialize the argument parser
+    parser = argparse.ArgumentParser(description="Process a percentage of URLs from a CSV file.")
+    
+    # Add arguments for the CSV file path and the percentage
+    parser.add_argument('save_path', type=str, help='Path to the output directory')
+    parser.add_argument('n_samples', type=float, help='Number of samples of the database to pull')
+
+    # Parse the arguments
+    args = parser.parse_args()
+
+
+    firmware_data_path = "/home/nathan/Documents/Data/Firmware-Dataset/dat/firmware_download_list.csv"
     save_path = "../fws"
-    main(firmware_urls, save_path)
+    main(firmware_data_path, save_path, args.n_samples)
     
